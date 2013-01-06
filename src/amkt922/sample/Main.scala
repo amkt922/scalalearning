e12c84aa3920608bda6c39538d646fb1a74a3794
amkt922 <amkt922@gmail.com>
2013-01-06 22:05:39 +0900

    add control structures

--- src/amkt922/sample/Main.scala
+++ src/amkt922/sample/Main.scala
@@ -8,7 +8,31 @@ package amkt922.sample
  *
  */
 object Main {
-	def main(args:Array[String]) = {
-	  println("hello scalalearning world.")
-	}
+  def main(args: Array[String]): Unit = {
+    println("hello scalalearning world.")
+    val m = new Main
+    println(m.isOdd(2))
+    println(m.isEven(2))
+    println(m.isLeapYear(1981))
+  }
+}
+
+class Main {
+  def isOdd(a: Int): Boolean = if (a % 2 == 1) true else false
+  def isEven(a: Int): Boolean = {
+    !isOdd(a)
+  }
+  def isLeapYear(year: Int): Boolean = {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        true
+      } else {
+        false
+      }
+    } else if (year % 4 == 0) {
+      true
+    } else {
+      false
+    }
+  }
 }
