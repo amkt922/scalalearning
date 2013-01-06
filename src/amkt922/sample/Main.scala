b461e11feb882e77a6449031848fa773a0aa07ea
amkt922 <amkt922@gmail.com>
2013-01-06 16:07:08 +0900

    add main object, and let it say 'hello world'

---
+++ src/amkt922/sample/Main.scala
@@ -0,0 +1,14 @@
+/**
+ * scalalearning sample program
+ */
+package amkt922.sample
+
+/**
+ * @author dev
+ *
+ */
+object Main {
+	def main(args:Array[String]) = {
+	  println("hello scalalearning world.")
+	}
+}
